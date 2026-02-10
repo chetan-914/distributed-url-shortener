2076c1421eb54b63324767174c5f19ed394398ce
chetan-914 <chetanjunja914@gmail.com>
2026-02-10 21:26:53 +0530

    Dockerize the app

--- app/core/config.py
+++ app/core/config.py
@@ -3,11 +3,11 @@ import os
 class Settings:
     DATABASE_URL: str = os.getenv(
         "DATABASE_URL",
-        "postgresql+psycopg2://url_user:url_pass@localhost:5432/url_shortener"
+        "postgresql+psycopg2://url_user:url_pass@postgres:5432/url_shortener"
     )
     REDIS_URL: str = os.getenv(
         "REDIS_URL",
-        "redis://localhost:6379"
+        "redis://redis:6379"
     )
 
 settings = Settings()
